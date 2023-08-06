169940670ce41c51141b8015f87dfcfe0c7e3c4f
nabil_khmissi <bilakhmissi@gmail.com>
2023-08-06 19:20:32 +0100

    add a class for beans

--- src/main/java/com/example/springSecurityFormBasedLogin/security/BeanDeclarations.java
+++ src/main/java/com/example/springSecurityFormBasedLogin/security/BeanDeclarations.java
@@ -1,12 +1,34 @@
 package com.example.springSecurityFormBasedLogin.security;
 
+import com.example.springSecurityFormBasedLogin.filter.TwoStepVerificationFilter;
+import com.example.springSecurityFormBasedLogin.interfaces.TotpManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class BeanDeclarations {
+
+    private final CustomUserDetailsService userDetailsService;
+    private final TotpManager totpManager;
+
+    @Bean
+    public CustomAuthenticationProvider authenticationProvider() {
+        CustomAuthenticationProvider authenticationProvider
+                = new CustomAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setMfaTokenManager(totpManager);
+        return authenticationProvider;
+    }
+
+    @Bean
+    public CustomAuthenticationFailureHandler authenticationFailureHandler(){
+        return new CustomAuthenticationFailureHandler();
+    }
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
