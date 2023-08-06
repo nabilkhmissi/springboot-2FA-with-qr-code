f896aaea588c1d76dd2188bc756584791ce5676c
nabil_khmissi <bilakhmissi@gmail.com>
2023-08-06 19:22:14 +0100

    extend security class to handle 2FA

--- src/main/java/com/example/springSecurityFormBasedLogin/security/SecurityConfig.java
+++ src/main/java/com/example/springSecurityFormBasedLogin/security/SecurityConfig.java
@@ -1,28 +1,24 @@
 package com.example.springSecurityFormBasedLogin.security;
 
+import com.example.springSecurityFormBasedLogin.filter.TwoStepVerificationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    private CustomDaoAuthenticationProvider authenticationProvider;
-    private CustomAuthenticationFailureHandler authenticationFailureHandler;
-    private CustomUserDetailsService userDetailsService;
-
-    public SecurityConfig(CustomDaoAuthenticationProvider authenticationProvider,
-                          CustomUserDetailsService userDetailsService,
-                          CustomAuthenticationFailureHandler authenticationFailureHandler
-                          ) {
-        this.authenticationProvider = authenticationProvider;
-        this.userDetailsService = userDetailsService;
-        this.authenticationFailureHandler = authenticationFailureHandler;
-    }
+    private final CustomAuthenticationFailureHandler authenticationFailureHandler;
+    private final CustomAuthenticationProvider authenticationProvider;
+    private final CustomWebAuthenticationDetailsSource authenticationDetailsSource;
+    private final TwoStepVerificationFilter twoStepVerificationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -31,21 +27,24 @@ public class SecurityConfig {
         http.csrf(csrf->csrf.disable());
         http
                 .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/two-step-verification").hasAuthority("TOTP_AUTH_AUTHORITY")
                         .requestMatchers("/login",
-                                "/h2-console/**",
-                                "/register",
-                                "/").permitAll()
-                        .anyRequest().authenticated());
+                        "/h2-console/**",
+                        "/register",
+                        "/").permitAll()
+                        .anyRequest().hasAnyRole("USER", "ADMIN"));
 
         http.formLogin(form -> form
                 .loginPage("/login")
                 .loginProcessingUrl("/login")
-                .defaultSuccessUrl("/users")
+                .defaultSuccessUrl("/")
+                .authenticationDetailsSource(authenticationDetailsSource)
                 .failureHandler(authenticationFailureHandler));
 
         http.logout(logout->logout
                 .logoutUrl("/logout")
                 .permitAll());
+        http.addFilterBefore(twoStepVerificationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
