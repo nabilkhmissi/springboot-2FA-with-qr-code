a3db82f2b44c624674bf2b1087604f2293df3689
nabil_khmissi <bilakhmissi@gmail.com>
2023-08-06 19:21:05 +0100

    delete unused class

--- src/main/java/com/example/springSecurityFormBasedLogin/security/CustomAuthenticationFailureHandler.java
+++ src/main/java/com/example/springSecurityFormBasedLogin/security/CustomAuthenticationFailureHandler.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-@Component
 public class CustomAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
     @Override
